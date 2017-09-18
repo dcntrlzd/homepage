a0050e28d5750254a7c1a0eacca0d778050f6c2e
Baris Gümüstas <baris.guemuestas@bonial.com>
2017-09-18 02:06:19 +0200

    Cleaning up and fixing up build config.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import Web3 from 'web3';
-import Graph from 'ngraph.graph';
-import Renderer from 'ngraph.pixel';
+import 'promise-polyfill';
+import './styles.css';
 
 let initialBlock;
 let latestBlock;
 let currentBlock;
-
-const provider = new Web3.providers.HttpProvider("https://mainnet.infura.io/mTYprP0XeHjUrSadUTc0");
-const web3 = new Web3(provider);
-const graph = Graph();
-let initialized = false;
 
 const baseInterval = 15 * 1000;
 const catchUpTime = 1 * 60 * 1000;
 const backInTime = 5 * 60 * 1000;
 const catchUpInterval = (catchUpTime / backInTime)  * baseInterval;
 
-web3.eth.getBlockNumber()
+const loadDependencies = () => Promise.all([
+  import(/* webpackChunkName: "web3" */ 'web3'),
+  import(/* webpackChunkName: "ngraph" */ 'ngraph.graph'),
+  import(/* webpackChunkName: "ngraph" */ 'ngraph.pixel'),
+]);
+
+loadDependencies().then(([ Web3, Graph, Renderer]) => {
+  const provider = new Web3.providers.HttpProvider("https://mainnet.infura.io/mTYprP0XeHjUrSadUTc0");
+  const web3 = new Web3(provider);
+  const graph = Graph();
+  
+  let initialized = false;
+
+  web3.eth.getBlockNumber()
   .then((_latestBlock) => {
     latestBlock = _latestBlock;
     initialBlock = latestBlock - Math.floor(backInTime / baseInterval);
@@ -24,40 +31,46 @@ web3.eth.getBlockNumber()
     fetch();
   });
 
-function fetch() {
-  web3.eth.getBlock(currentBlock >= latestBlock ? 'latest' : currentBlock)
-    .then(({ number, transactions: transactionIds }) => {
-      currentBlock = number;
-      return Promise.all(transactionIds.map((transactionId) => {
-        return web3.eth.getTransactionReceipt(transactionId);
-      }));
-    })
-    .then((transactions) => {
-      const counter = (currentBlock !== initialBlock) ? `#${initialBlock} - #${currentBlock}` : `#${currentBlock}`;
-      document.querySelector('.counter').innerHTML = `ETHERERUM BLOCK ${counter}`;
-      currentBlock += 1;
+  function fetch() {
+    web3.eth.getBlock(currentBlock >= latestBlock ? 'latest' : currentBlock)
+      .then(({ number, transactions: transactionIds }) => {
+        currentBlock = number;
+        return Promise.all(transactionIds.map((transactionId) => {
+          return web3.eth.getTransactionReceipt(transactionId);
+        }));
+      })
+      .then((transactions) => {
+        const currentBlockLink = `<a href="https://etherscan.io/txs?block=${currentBlock}" target="_blank">#${currentBlock}</a>`;
+        const counter = (currentBlock !== initialBlock) ? `#${initialBlock} -> ${currentBlockLink}` : `${currentBlockLink}`;
+        document.querySelector('.counter').innerHTML = `ETHERERUM BLOCK ${counter}</a>`;
+        currentBlock += 1;
 
-      graph.beginUpdate();
-      transactions.forEach((transaction) => {
-        const { from, to, contractAddress } = transaction;
-        const params = [from || contractAddress, to || contractAddress, {}];
-        graph.addLink(...params);
-      });
-      graph.endUpdate();
-
-      if (!initialized && graph.getLinksCount() > 0) {
-        const renderer = Renderer(graph, {
-          clearColor: 0x333333,
-          autoFit: true,
-          container: document.getElementById('graph'),
-          is3d: true,
-          link: () => ({ fromColor: 0x666666, toColor: 0x666666 }),
-          node: () => ({ color: 0x666666, size: 10 })
+        graph.beginUpdate();
+        transactions.forEach((transaction) => {
+          const { from, to, contractAddress } = transaction;
+          const params = [from || contractAddress, to || contractAddress, {}];
+          graph.addLink(...params);
         });
-        initialized = true;
-      }
+        graph.endUpdate();
 
-      const timeout = (currentBlock >= latestBlock) ? baseInterval : catchUpInterval;
-      setTimeout(fetch, timeout);
-    });
-}
+        if (!initialized && graph.getLinksCount() > 0) {
+          const renderer = Renderer(graph, {
+            clearColor: 0x333333,
+            autoFit: true,
+            container: document.getElementById('graph'),
+            is3d: true,
+            link: () => ({ fromColor: 0x666666, toColor: 0x666666 }),
+            node: () => ({ color: 0x666666, size: 10 })
+          });
+
+          console.log(renderer);
+          // this.renderer.setPixelRatio(window.devicePixelRatio);
+
+          initialized = true;
+        }
+
+        const timeout = (currentBlock >= latestBlock) ? baseInterval : catchUpInterval;
+        setTimeout(fetch, timeout);
+      });
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,52 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new ExtractTextPlugin('main.[contenthash].css'),
+  ],
   module: {
     rules: [
+      {
+        test: /\.(css)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader' },
+            { 
+              loader: 'postcss-loader',
+              options: {
+                plugins: (loader) => [
+                  require('autoprefixer')(),
+                ]
+              }
+            },
+          ],
+        })
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+        }
+      },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
           options: {
+            plugins: ['syntax-dynamic-import'],
             presets: ['env']
           }
         }
